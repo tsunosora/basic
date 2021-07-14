084caba7f908e097b6de69cbdf704c2f56a6e3ab
tsunosora <muhamadfaisal288@gmail.com>
2021-07-14 11:23:20 +0700

    Query Builder Edit & Update Data

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -59,15 +59,22 @@ class CategoryController extends Controller
     }
 
     public function Edit($id){
-        $categories = Category::find($id);
+        // $categories = Category::find($id);
+        $categories = DB::table('categories')->where('id',$id)->first();
         return view('admin.category.edit',compact('categories'));
     }
 
     public function update(Request $request, $id){
-        $update = Category::find($id)->update([
-            'category_name' => $request->category_name,
-            'user_id' => Auth::user()->id
-        ]);
+        // $update = Category::find($id)->update([
+        //     'category_name' => $request->category_name,
+        //     'user_id' => Auth::user()->id
+        // ]);
+
+        $data = array();
+        $data['category_name'] = $request->category_name;
+        $data['user_id'] = Auth::user()->id;
+        DB::table('categories')->where('id',$id)->update($data);
+
 
         return Redirect()->route('all.category')->with('success','Category Berhasil di Update!!');
 
