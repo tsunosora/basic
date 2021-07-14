1b056a04a85e3b8b23f2678eee1bf8669c3c9863
tsunosora <muhamadfaisal288@gmail.com>
2021-07-14 09:47:06 +0700

    Query Builder Join Table

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -12,7 +12,13 @@ use Illuminate\Support\Facades\DB;
 class CategoryController extends Controller
 {
     public function AllCat(){
-        $categories = Category::latest()->paginate(5);
+        $categories = DB::table('categories')
+        ->join('users','categories.user_id','users.id')
+        ->select('categories.*','users.name')
+        ->latest()->paginate(5);
+
+
+        // $categories = Category::latest()->paginate(5);
         // $categories = DB::table('categories')->latest()->paginate(5);
         return view('admin.category.index', compact('categories'));
     }
